2f7b4e179d902d2a286f26378859e4fdb61b81ad
narupi <narupi1999@gmail.com>
2021-08-27 19:13:18 +0900

    Add: 構造体制御用関数の追加

--- src/domain/site.go
+++ src/domain/site.go
@@ -1,5 +1,9 @@
 package domain
 
+import "fmt"
+
+const InvalidURL = "invalid empty url string"
+
 type Site struct {
 	id  int
 	url string
@@ -7,3 +11,22 @@ type Site struct {
 }
 
 type Sites []Site
+
+func NewSite(i int, u string, g bool) (Site, error) {
+	if u == "" {
+		return Site{}, fmt.Errorf(InvalidURL)
+	}
+	return Site{id: i, url: u, gpc: g}, nil
+}
+
+func (s Site) ID() int {
+	return s.id
+}
+
+func (s Site) URL() string {
+	return s.url
+}
+
+func (s Site) GPC() bool {
+	return s.gpc
+}
