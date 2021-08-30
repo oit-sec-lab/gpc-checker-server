7b15c89a7c77f015149280593cbcd01e3f9fca03
narupi <narupi1999@gmail.com>
2021-08-31 07:24:40 +0900

    Add: usecase tests

--- src/usecase/site.go
+++ src/usecase/site.go
@@ -1,30 +1,51 @@
 package usecase
 
 import (
+	"fmt"
 	"github.com/oit-sec-lab/dnt-verify-server/src/domain/entities"
 	"github.com/oit-sec-lab/dnt-verify-server/src/domain/repositories"
 )
 
 type SiteInteractor struct {
-	SiteRepository repositories.ISiteRepository
+	siteRepository repositories.ISiteRepository
 }
 
+const (
+	URLNotFound = "url not found"
+)
+
+func NewSiteInteractor(r repositories.ISiteRepository) SiteInteractor {
+	return SiteInteractor{r}
+}
 func (interactor *SiteInteractor) Add(s entities.Site) (err error) {
-	err = interactor.SiteRepository.Store(s)
+	err = interactor.siteRepository.Store(s)
 	return
 }
 
 func (interactor *SiteInteractor) FindByURL(u string) (Site entities.Site, err error) {
-	Site, err = interactor.SiteRepository.FindByURL(u)
+	exist, e := interactor.siteRepository.Exists(u)
+	if e != nil {
+		return entities.Site{}, e
+	}
+	if !exist {
+		return entities.Site{}, fmt.Errorf(URLNotFound)
+	}
+	Site, err = interactor.siteRepository.FindByURL(u)
 	return
 }
 
 func (interactor *SiteInteractor) VerifyGPC(u string) (gpc bool, err error) {
-	Site, err := interactor.SiteRepository.FindByURL(u)
-	if err != nil {
-		return interactor.SiteRepository.CheckGPC(Site.URL())
-	} else {
-		Site.GPC()
+	exist, e := interactor.siteRepository.Exists(u)
+	if e != nil {
+		return false, e
 	}
-	return
+	if exist {
+		s, e := interactor.siteRepository.FindByURL(u)
+		if e != nil {
+			return false, e
+		}
+		return s.GPC(), e
+	} else {
+		return interactor.siteRepository.CheckGPC(u)
+	}
 }
