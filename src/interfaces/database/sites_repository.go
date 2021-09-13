6837c2cdb4a68daa9e3d86fcef00e3e1c234eae0
tami <tamio3141@outlook.jp>
2021-09-14 00:11:20 +0900

    use Site getter setter

--- src/interfaces/database/sites_repository.go
+++ src/interfaces/database/sites_repository.go
@@ -1,14 +1,17 @@
 package database
 
-import "github.com/oit-sec-lab/dnt-verify-server/src/domain/entities/site"
+import (
+    "github.com/oit-sec-lab/dnt-verify-server/src/domain/entities/site"
+    "github.com/oit-sec-lab/dnt-verify-server/src/domain/entities/gpc"
+)
 
 type SiteRepository struct {
     SqlHandler
 }
 
 func (repo *SiteRepository) Store(s site.Site) (err error) {
-    result, err := repo.Execute(
-        "INSERT INTO sites (url, gpc) VALUES (?,?)", s.url, s.gpc,
+    _, err = repo.Execute(
+        "INSERT INTO sites (url, gpc) VALUES (?,?)", s.URL(), s.GPC(),
     )
     if err != nil {
         return
@@ -24,24 +27,28 @@ func (repo *SiteRepository) FindByURL(identifier string) (s site.Site, err error
     }
     var id int
     var url string
-    var gpc bool
+    var gpc gpc.Gpc
     row.Next()
     if err = row.Scan(&id, &url, &gpc); err != nil {
         return
     }
-    s.id = id
-    s.url = url
-    s.gpc = gpc
+
+    s, err = site.NewSite(id, url, gpc)
+    if err != nil {
+        return
+    }
     return
 }
 
 func (repo *SiteRepository) Exists(identifier string) (find bool, err error) {
-    row, err := repo.Query("SELECT id FROM sites WHERE url = ?", identifier)
+	row, err := repo.Query("SELECT id FROM sites WHERE url = ?", identifier)
     if err != nil {
         return
     }
+    defer row.Close()
 
-    if row.Next() {
+    //if result had row, row.Next() would be true
+	if row.Next() {
         find = true
     } else {
         find = false
